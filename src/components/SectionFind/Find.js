114b78a76083ba18b81937bbdb34f5890687d323
Mariusz Kaczmarek <mariuszkaczmrek97@outlook.com>
2020-06-13 14:21:22 +0200

    Added forms

--- src/components/SectionFind/Find.js
+++ src/components/SectionFind/Find.js
@@ -16,29 +16,14 @@ class Find extends React.Component {
 
 
         return (
-            <>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">id</th>
-                            <th scope="col">name</th>
-                            <th scope="col">Count invoices</th>
-                            <th scope="col">International Transport</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.companies.length === 0 ? "Nic" : this.state.companies.map(item => (
-                            <DataTable
-                                data={item}
-                            />))}
 
-                    </tbody>
-                </table>
+            <form action="" className="form-inline mt-3">
+                <input type="number" class="form-control" id="inputPassword2" placeholder="Password" />
+                <button type="submit" className="btn btn-primary ">SEARCH</button>
 
-            </>
+            </form>
         )
     }
-}
 
+}
 export default Find;
--- src/components/SectionUpdate/Update.js
+++ src/components/SectionUpdate/Update.js
@@ -29,7 +29,35 @@ class Update extends React.Component {
     render() {
         return (
             <>
-                <p>USuwanie</p>
+                <form>
+                    <div class="form-group row">
+                        <label for="name" class="col-sm-2 col-form-label">Company Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="name" placeholder="Company Name" />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="invoices" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="invoices" placeholder="Amount" />
+                            <input type="text" class="form-control" id="invoices" placeholder="Name" />
+                            <input type="text" class="form-control" id="invoices" placeholder="Surrname" />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2"> International Transport</div>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="International" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </form>
             </>
         )
     }
